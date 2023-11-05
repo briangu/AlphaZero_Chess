92a166915ab4d5c445f367dfe2b28e666e2c3654
Brian Guarraci <brian.guarraci@gmail.com>
2023-11-05 13:18:02 -0700

    add queue size to tobj

--- src/pgn_w_eval.py
+++ src/pgn_w_eval.py
@@ -163,14 +163,14 @@ def save_pgn_to_db(pgn_path, game_cnt, num_workers):
     for w in workers:
         w.start()
 
-    game_threshold = 20000*num_workers
+    game_threshold = 2000*num_workers
     write_threshold = 1000000
     tobj = tqdm(enumerate(read_games(pgn_path)), total=game_cnt)
     for i, raw_pgn in tobj:
         game_queue.put(raw_pgn)
         wq = write_queue.qsize()
         gq = game_queue.qsize()
-        if gq > game_threshold or wq > write_threshold:
+        while gq > game_threshold or wq > write_threshold:
             time.sleep(0.1)
         if i % 100 == 0:
             tobj.set_postfix({"game_queue": gq, "write_queue": wq}, refresh=True)
