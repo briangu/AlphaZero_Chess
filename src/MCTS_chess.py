8020aa17c138ae34f4f64f4e4f380f1bf5b293c3
Brian Guarraci <brian.guarraci@gmail.com>
2023-11-05 09:59:26 -0700

    use multi-gpu

--- src/MCTS_chess.py
+++ src/MCTS_chess.py
@@ -17,13 +17,13 @@ class UCTNode():
         self.game = game # state s
         self.move = move # action index
         self.is_expanded = False
-        self.parent = parent  
+        self.parent = parent
         self.children = {}
         self.child_priors = np.zeros([4672], dtype=np.float32)
         self.child_total_value = np.zeros([4672], dtype=np.float32)
         self.child_number_visits = np.zeros([4672], dtype=np.float32)
         self.action_idxes = []
-        
+
     @property
     def number_visits(self):
         return self.parent.child_number_visits[self.move]
@@ -31,22 +31,22 @@ class UCTNode():
     @number_visits.setter
     def number_visits(self, value):
         self.parent.child_number_visits[self.move] = value
-    
+
     @property
     def total_value(self):
         return self.parent.child_total_value[self.move]
-    
+
     @total_value.setter
     def total_value(self, value):
         self.parent.child_total_value[self.move] = value
-    
+
     def child_Q(self):
         return self.child_total_value / (1 + self.child_number_visits)
-    
+
     def child_U(self):
         return math.sqrt(self.number_visits) * (
             abs(self.child_priors) / (1 + self.child_number_visits))
-    
+
     def best_child(self):
         if self.action_idxes != []:
             bestmove = self.child_Q() + self.child_U()
@@ -54,20 +54,20 @@ class UCTNode():
         else:
             bestmove = np.argmax(self.child_Q() + self.child_U())
         return bestmove
-    
+
     def select_leaf(self):
         current = self
         while current.is_expanded:
           best_move = current.best_child()
           current = current.maybe_add_child(best_move)
         return current
-    
+
     def add_dirichlet_noise(self,action_idxs,child_priors):
         valid_child_priors = child_priors[action_idxs] # select only legal moves entries in child_priors array
         valid_child_priors = 0.75*valid_child_priors + 0.25*np.random.dirichlet(np.zeros([len(valid_child_priors)], dtype=np.float32)+0.3)
         child_priors[action_idxs] = valid_child_priors
         return child_priors
-    
+
     def expand(self, child_priors):
         self.is_expanded = True
         action_idxs = []; c_p = child_priors
@@ -84,7 +84,7 @@ class UCTNode():
         if self.parent.parent == None: # add dirichlet noise to child_priors in root node
             c_p = self.add_dirichlet_noise(action_idxs,c_p)
         self.child_priors = c_p
-    
+
     def decode_n_move_pieces(self,board,move):
         i_pos, f_pos, prom = ed.decode_action(board,move)
         for i, f, p in zip(i_pos,f_pos,prom):
@@ -105,8 +105,8 @@ class UCTNode():
                     board.player = self.game.player
                     board.move_piece((0,0),(0,3),None)
         return board
-            
-    
+
+
     def maybe_add_child(self, move):
         if move not in self.children:
             copy_board = copy.deepcopy(self.game) # make copy of board
@@ -114,7 +114,7 @@ class UCTNode():
             self.children[move] = UCTNode(
               copy_board, move, parent=self)
         return self.children[move]
-    
+
     def backup(self, value_estimate: float):
         current = self
         while current.parent is not None:
@@ -124,7 +124,7 @@ class UCTNode():
             elif current.game.player == 0: # same as current.parent.game.player = 1
                 current.total_value += (-1*value_estimate)
             current = current.parent
-        
+
 
 class DummyNode(object):
     def __init__(self):
@@ -133,7 +133,9 @@ class DummyNode(object):
         self.child_number_visits = collections.defaultdict(float)
 
 
-def UCT_search(game_state, num_reads,net):
+def UCT_search(game_state, num_reads, net, gpu_id):
+    torch.cuda.set_device(gpu_id)
+    net.to(f'cuda:{gpu_id}')
     root = UCTNode(game_state, move=None, parent=DummyNode())
     for i in range(num_reads):
         leaf = root.select_leaf()
@@ -187,7 +189,7 @@ def load_pickle(filename):
     return data
 
 
-def MCTS_self_play(chessnet,num_games,cpu):
+def MCTS_self_play(chessnet,num_games,cpu,gpu_id):
     for idxx in range(0,num_games):
         current_board = c_board()
         checkmate = False
@@ -203,7 +205,7 @@ def MCTS_self_play(chessnet,num_games,cpu):
                 break
             states.append(copy.deepcopy(current_board.current_board))
             board_state = copy.deepcopy(ed.encode_board(current_board))
-            best_move, root = UCT_search(current_board,777,chessnet)
+            best_move, root = UCT_search(current_board,777,chessnet,gpu_id)
             current_board = do_decode_n_move_pieces(current_board,best_move) # decode move and move piece(s)
             policy = get_policy(root)
             dataset.append([board_state,policy])
@@ -214,7 +216,7 @@ def MCTS_self_play(chessnet,num_games,cpu):
                 elif current_board.player == 1: # white wins
                     value = 1
                 checkmate = True
-                
+
         dataset_p = []
         for idx,data in enumerate(dataset):
             s,p = data
@@ -226,9 +228,9 @@ def MCTS_self_play(chessnet,num_games,cpu):
         save_as_pickle("dataset_cpu%i_%i_%s" % (cpu,idxx, datetime.datetime.today().strftime("%Y-%m-%d")),dataset_p)
 
 
-    
+
 if __name__=="__main__":
-    
+
     net_to_play="current_net_trained8_iter1.pth.tar"
     mp.set_start_method("spawn",force=True)
     net = ChessNet()
@@ -238,16 +240,14 @@ if __name__=="__main__":
     net.share_memory()
     net.eval()
     print("hi")
-    #torch.save({'state_dict': net.state_dict()}, os.path.join("./model_data/",\
-    #                                "current_net.pth.tar"))
-    
-    current_net_filename = os.path.join("./model_data/",\
-                                    net_to_play)
+    current_net_filename = os.path.join("./model_data/", net_to_play)
+    torch.save({'state_dict': net.state_dict()}, current_net_filename)
     checkpoint = torch.load(current_net_filename)
     net.load_state_dict(checkpoint['state_dict'])
     processes = []
-    for i in range(6):
-        p = mp.Process(target=MCTS_self_play,args=(net,50,i))
+    for i in range(12):
+        gpu_id = i % 2
+        p = mp.Process(target=MCTS_self_play,args=(net,50,i,gpu_id))
         p.start()
         processes.append(p)
     for p in processes:
